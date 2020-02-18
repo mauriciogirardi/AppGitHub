0e68a9ae5ae246a32cfffc58e565f9336ad8010b
mauricio <maurigirarde@yahoo.com.br>
2020-02-18 18:13:59 -0300

    app github

---
+++ src/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+import { render } from "react-dom";
+import React from "react";
+import { AppContainer } from "react-hot-loader";
+import App from "./App";
+
+const renderApp = NextApp => {
+  render(
+    <AppContainer>
+      <NextApp />
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  );
+};
+
+renderApp(App);
+
+if (module.hot) {
+  module.hot.accept("./App.js", () => {
+    const NextApp = require("./App").default;
+    renderApp(NextApp);
+  });
+}
